4e0a1ace551a4f5e6e55766af9e05ff8cc1fd385
Bright <bright.sablah@outlook.com>
2023-09-09 03:29:11 +0000

    added a pogram that prints all possible combinations of two digits

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+
+/**
+  * main- Entry point
+  *
+  * Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+	int num1;
+	int num2;
+
+	num1 = 0;
+	while (num1 <= 9)
+	{
+		num2 = 0;
+		while (num2 <= 9)
+		{
+			if (num1 != num2 && num1 < num2)
+			{
+				putchar(num1 + 48);
+				putchar(num2 + 48);
+
+				if (num1 + num2 != 17)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+			num2++;
+		}
+		num1++;
+	}
+	putchar('\n');
+
+
+	return (0);
+}
