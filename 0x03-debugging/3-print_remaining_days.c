8c94dee039a329cfc30aaf0523c00074bfc04480
Bright <bright.sablah@outlook.com>
2023-09-13 21:24:29 +0000

    task 3 checker 9 fix

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -3,7 +3,7 @@
 
 /**
 * print_remaining_days - takes a date and prints how many days are
-* left in the year, taking leap years into account
+*	left in the year, taking leap years into account
 * @month: month in number format
 * @day: day of month
 * @year: year
@@ -12,7 +12,7 @@
 
 void print_remaining_days(int month, int day, int year)
 {
-	if ((year % 4 == 0 || year % 400 == 0) || (year % 100 == 0))
+	if ((year % 100 == 0 && year % 400 == 0) || (year % 4 == 0))
 	{
 		if (month >= 2 && day >= 60)
 		{
