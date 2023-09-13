281c8456e128a753eec4a08f797f17c2d6f1eccb
Bright <bright.sablah@outlook.com>
2023-09-13 03:58:30 +0000

    task 8 checker 5 debugging code

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -9,29 +9,22 @@
 
 void jack_bauer(void)
 {
-int min1;
-int min2;
-int hr1;
-int hr2;
 
-	for (hr1 = 0; hr1 <= 2; hr1++)
+int totalMinutes;
+
+	for (totalMinutes = 0; totalMinutes < 1440; totalMinutes++)
 	{
-		for (hr2 = 0; hr2 <= 3; hr2++)
-		{
-			for (min1 = 0; min1 <= 5; min1++)
-			{
-				for (min2 = 0; min2 <= 9; min2++)
-				{
-					_putchar(hr1 + '0');
-					_putchar(hr2 + '0');
-					_putchar(':');
-					_putchar(min1 + '0');
-					_putchar(min2 + '0');
-					_putchar('\n');
-				}
-			}
-		}
+		int hours = totalMinutes / 60;
+		int minutes = totalMinutes % 60;
+
+		_putchar((hours / 10) + '0');
+		_putchar((hours % 10) + '0');
+		_putchar(':');
+		_putchar((minutes / 10) + '0');
+		_putchar((minutes % 10) + '0');
+
+		_putchar('\n');
+
 
 	}
-
 }
