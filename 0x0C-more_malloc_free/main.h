46ba79eddbfafe46fd911e9389ab356caae6cc47
Bright <bright.sablah@outlook.com>
2023-10-05 17:38:35 +0000

    added main header file - cause of all this problems

---
+++ 0x0C-more_malloc_free/main.h
@@ -0,0 +1,22 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+int _putchar(char c);
+void *malloc_checked(unsigned int n);
+int _strlen_recursion(char *s);
+char *string_nconcat(char *s1, char *s2, unsigned int n);
+void *_calloc(unsigned int nmemb, unsigned int size);
+int *array_range(int min, int max);
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
+void _puts(char *str);
+void strmul(char *num1, char *num2, char *result);
+int _strlen(char *s);
+void *int_calloc(int nmemb, int size);
+
+void str_mul(char *num1, char *num2, int len1, int len2, int *mul_result, char *result);
+void *my_calloc(int nmemb, int size);
+int _strlen(char *s);
+int _putchar(char c);
+void _puts(char *str);
+
+#endif
