a990205dae574e3e74cc93383a468d2dc29bb880
Bright <bright.sablah@outlook.com>
2023-10-10 05:16:27 +0000

    added if statement to only free memory if pointer is not NULL

--- 0x0E-structures_typedef/5-free_dog.c
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -1,5 +1,6 @@
 #include "dog.h"
 #include <stdlib.h>
+#include <stddef.h>
 /**
 * free_dog - frees alocated memory
 * Description:
@@ -10,7 +11,10 @@
 
 void free_dog(dog_t *d)
 {
+	if (d != NULL)
+	{
 	free(d->name);
 	free(d->owner);
 	free(d);
+	}
 }
