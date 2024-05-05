e415a595e8d569d9b433d5a2ea7365ee7a8446fa
Bright <bright.sablah@outlook.com>
2024-05-05 11:29:07 +0000

    list len

--- 0x17-doubly_linked_lists/1-dlistint_len.c
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -1,11 +1,11 @@
 #include "lists.h"
 
 /**
- * print_dlistint - Prints all elements of a dlistint_t list
+ * dlistint_len - return number of elements of a dlistint_t list
  * @h: Pointer to the head of the list
  * Return: The number of nodes
  */
-size_t print_dlistint(const dlistint_t *h)
+size_t dlistint_len(const dlistint_t *h)
 {
 	size_t node_count = 0;
 
